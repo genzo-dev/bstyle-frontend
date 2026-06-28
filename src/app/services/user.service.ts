976147ce4eacb38823d1a3619c7941201e63a332
genzo-dev <gabrielenzopdf12@outlook.com>
2026-06-28 11:58:05 -0300

    feat(user): add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../types/user.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/auth/registrar`, data);
+  }
+
+  getUsers(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/usuarios/perfil`);
+  }
+}
